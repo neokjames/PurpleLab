61f4dbc8cd8cd12e7b6255f269850204b0c0e4da
James Campbell <neokjames@gmail.com>
2020-08-16 17:30:15 +1000

    Add Splunk UF TAs
    Include playbook steps and supporting files to install Splunk UF and TAs

---
+++ Vagrant/scripts/install-splunkuf-sysmon_ta.ps1
@@ -0,0 +1,28 @@
+# Purpose: Installs the Splunk Universal Forwarder Sysmon Technical Add-On
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing the Sysmon TA for Splunk"
+
+If (Test-Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-microsoft-sysmon\default") {
+  Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Sysmon TA is already installed. Moving on."
+  Exit 0
+}
+
+# Install Sysmon TA
+$sysmontaPath = "C:\vagrant\resources\splunk_forwarder\splunk-add-on-for-microsoft-sysmon_1062.tgz"
+$inputsPath = "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-microsoft-sysmon\local\inputs.conf"
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Installing the Sysmon TA"
+Start-Process -FilePath "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" -ArgumentList "install app $sysmontaPath -auth admin:changeme" -NoNewWindow
+
+# Create local directory
+New-Item -ItemType Directory -Force -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-microsoft-sysmon\local"
+Copy-Item c:\vagrant\resources\splunk_forwarder\sysmon_ta_inputs.conf $inputsPath -Force
+
+# Add a check here to make sure the TA was installed correctly
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Sleeping for 15 seconds"
+Start-Sleep -s 15
+If (Test-Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-microsoft-sysmon\default") {
+  Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Sysmon TA installed successfully."
+} Else {
+  Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Something went wrong during installation."
+  exit 1
+}
